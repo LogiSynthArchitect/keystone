4a0f5bb10bc16c955ddba8578b5e270956806cd3
LogiSynthArchitect <cybocrime@gmail.com>
2026-03-17 21:42:26 +0000

    chore: add build script for Vercel to bypass character limit

---
+++ scripts/vercel_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git --depth 1 -b stable
+fi
+./flutter/bin/flutter build web --release --target lib/main_web.dart \
+  --dart-define=SUPABASE_URL=$SUPABASE_URL \
+  --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
