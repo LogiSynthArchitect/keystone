fd3b113bafa5771a7bd8e3e3348a3b9d4d10e37b
LogiSynthArchitect <cybocrime@gmail.com>
2026-03-17 21:51:16 +0000

    fix: add --no-wasm-dry-run to Vercel build script

--- scripts/vercel_build.sh
+++ scripts/vercel_build.sh
@@ -2,6 +2,6 @@
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 fi
-./flutter/bin/flutter build web --release --target lib/main_web.dart \
+./flutter/bin/flutter build web --release --target lib/main_web.dart --no-wasm-dry-run \
   --dart-define=SUPABASE_URL=$SUPABASE_URL \
   --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
